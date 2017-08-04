cb9e9214aca35ac22f498740a5e8e8192b2ebd5e
Slava <viacheslav.tereshchenko@gmail.com>
2017-08-04 18:52:58 +0200

    previous attempt to compile depending on predefined macros failed. Her configured main.cpp for ubuntu.

--- src/main.cpp
+++ src/main.cpp
@@ -185,27 +185,16 @@ int main()
   });
 
   int port = 4567;
-  #ifdef linux
-    if (h.listen(port)) // for ubuntu
-    {
-      std::cout << "Listening to port " << port << std::endl;
-    }
-    else
-    {
-      std::cerr << "Failed to listen to port" << std::endl;
-      return -1;
-    } 
-  #else
-    if (h.listen("127.0.0.1", port)) // for windows
-    {
-      std::cout << "Listening to port " << port << std::endl;
-    }
-    else
-    {
-      std::cerr << "Failed to listen to port" << std::endl;
-      return -1;
-    }
-  #endif
+  //if (h.listen("127.0.0.1", port)) // for windows
+  if (h.listen(port)) // for ubuntu
+  {
+    std::cout << "Listening to port " << port << std::endl;
+  }
+  else
+  {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  } 
   
   h.run();
 }
